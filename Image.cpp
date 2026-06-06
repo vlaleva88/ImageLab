4c2215ef26fd7b60a5a8a2fe1bc188975a812af6
Vanina Laleva <vaninaal888@gmail.com>
2026-06-06 16:28:51 +0300

    Add base class

---
+++ Image.cpp
@@ -0,0 +1,6 @@
+#include "Image.h"
+
+Image::Image(size_t width, size_t height) :
+                width(width), height(height) {
+    // bytes.resize(width * height * 3);
+}
---
+++ Image.h
@@ -0,0 +1,42 @@
+#ifndef IMAGELAB_IMAGE_H
+#define IMAGELAB_IMAGE_H
+
+#include<iostream>
+#include <vector>
+
+struct RGB {
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
+
+    RGB(unsigned char red, unsigned char green,
+        unsigned char blue) : red(red), green(green), blue(blue) {
+    }
+};
+
+enum class ImageType {
+    PBM = 4,
+    PGM,
+    PPM,
+    NOT_SUPPORTED,
+};
+
+
+class Image {
+protected:
+    size_t width;
+    size_t height;
+    uint16_t maxVal = 255;
+
+public:
+    Image(size_t width, size_t height);
+
+    virtual size_t getWidth() const = 0;
+    virtual size_t getHeight() const = 0;
+    virtual size_t getSize() const = 0;
+
+    virtual ~Image() = default;
+};
+
+
+#endif //IMAGELAB_IMAGE_H
